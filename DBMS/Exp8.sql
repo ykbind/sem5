3a515494c6756c89f4b159944f8f655d0b699d7a
Yash Bankar <81851279+ykbind@users.noreply.github.com>
2025-10-28 22:18:52 +0530

    Delete DBMS directory

--- DBMS/Exp8.sql
+++
@@ -1,9 +0,0 @@
-create database exp8;
-
-CREATE USER 'kaustubh'@'localhost' IDENTIFIED BY 'Strong@password';
-GRANT ALL PRIVILEGES ON exp8.* TO 'kaustubh'@'localhost';
-
-use exp8;
-
-create table Student(PRN int auto_increment primary key,Name varchar(20));
-
