d148911faf9ccc278427ca5c914b1f6932d4b54a
Yash Bankar <81851279+ykbind@users.noreply.github.com>
2025-11-06 19:19:53 +0530

    Refactor SQL queries and add sample data

--- DBMS/Exp16_exam.sql
+++ DBMS/Exp16_exam.sql
@@ -11,18 +11,7 @@ db.students.insertMany([
   { name: "Rohan", department: "Computer", marks: 75, city: "Pune" },
   { name: "Tina", department: "Mechanical", marks: 95, city: "Nashik" }
 ]);
-/*{
-  acknowledged: true,
-  insertedIds: {
-    '0': ObjectId('690a4f616dda139e99cebeb7'),
-    '1': ObjectId('690a4f616dda139e99cebeb8'),
-    '2': ObjectId('690a4f616dda139e99cebeb9'),
-    '3': ObjectId('690a4f616dda139e99cebeba'),
-    '4': ObjectId('690a4f616dda139e99cebebb'),
-    '5': ObjectId('690a4f616dda139e99cebebc'),
-    '6': ObjectId('690a4f616dda139e99cebebd')
-  }
-}*/
+
 
 // Q1. •	Calculate average marks per department using aggregation.
 
@@ -35,11 +24,7 @@ db.students.aggregate([
   }
 ]);
 
-/*[
-  { _id: 'Mechanical', avg_marks: 91.5 },
-  { _id: 'Computer', avg_marks: 84 },
-  { _id: 'IT', avg_marks: 73.5 }
-]*/
+
 
 // Q2. •	Find the highest marks in each department. 
 db.students.aggregate([
@@ -51,11 +36,6 @@ db.students.aggregate([
   }
 ]);
 
-/*[
-  { _id: 'IT', highest_marks: 78 },
-  { _id: 'Computer', highest_marks: 92 },
-  { _id: 'Mechanical', highest_marks: 95 }
-]*/
 
 // Q3. Find the sum of  marks in each department
 db.students.aggregate([
@@ -67,11 +47,7 @@ db.students.aggregate([
   }
 ]);
 
-/*[
-  { _id: 'Mechanical', total_marks: 183 },
-  { _id: 'Computer', total_marks: 252 },
-  { _id: 'IT', total_marks: 147 }
-]*/
+
 
 // Q4. Count the number of students from each city
 db.students.aggregate([
@@ -82,12 +58,7 @@ db.students.aggregate([
     }
   }
 ]);
-/*[
-[
-  { _id: 'Mumbai', student_count: 2 },
-  { _id: 'Pune', student_count: 3 },
-  { _id: 'Nashik', student_count: 2 }
-]*/
+
 
 // Q5. Calculate sum of  marks per department using aggregation arrange result in ascending order.
 db.students.aggregate([
@@ -100,26 +71,13 @@ db.students.aggregate([
   { $sort: { total_marks: 1 } }
 ]);
 
-/*[
-  { _id: 'IT', total_marks: 147 },
-  { _id: 'Mechanical', total_marks: 183 },
-  { _id: 'Computer', total_marks: 252 }
-]*/
 
 // Q6. Create single and composite index on the department field to optimize queries
 db.students.createIndex({ department: 1 });
-department_1
 
 db.students.createIndex({ department: 1, city: 1 });
-department_1_city_1
+
 
 db.students.getIndexes();
-[
-  { v: 2, key: { _id: 1 }, name: '_id_' },
-  { v: 2, key: { department: 1 }, name: 'department_1' },
-  {
-    v: 2,
-    key: { department: 1, city: 1 },
-    name: 'department_1_city_1'
-  }
-]
+
+
