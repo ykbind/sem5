2247d82dba880260d9220e0cb595afc68711c13b
Yash Bankar <81851279+ykbind@users.noreply.github.com>
2025-11-06 18:45:29 +0530

    Update Exp10_exam.sql

--- DBMS/Exp10_exam.sql
+++ DBMS/Exp10_exam.sql
@@ -1,9 +1,9 @@
-create database exp8;
+create database exp;
 
 CREATE USER 'kaustubh'@'localhost' IDENTIFIED BY 'Strong@password';
 GRANT ALL PRIVILEGES ON exp8.* TO 'kaustubh'@'localhost';
 
-use exp8;
+use exp;
 
 create table Student(PRN int auto_increment primary key,Name varchar(20));
 
